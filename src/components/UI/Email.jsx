023be687c0009c83f069924588503f173d9ebca0
Harsh Sharma <120568101+HarshSharmaIN@users.noreply.github.com>
2024-05-08 22:40:09 +0530

    Update Email.jsx

--- src/components/UI/Email.jsx
+++ src/components/UI/Email.jsx
@@ -40,8 +40,8 @@ export default function Email({ order }) {
           <Section className="mt-[32px]">
             <Img
                 src={`${import.meta.env.VITE_BASE_URL}/assets/logo-BG5OLuJH.jpg`}
-                width="40"
-                height="37"
+                width="200"
+                height="200"
                 alt="React Restaurant"
               />
             <Heading as="h2">Thank You for ordering from us:</Heading>
